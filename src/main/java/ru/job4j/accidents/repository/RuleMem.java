6afe9e2dc542a8a3667727ba75784c7ecb51eac9
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-12-03 08:09:51 +0300

    5. Form с агрегационными объектами [#305523]

--- src/main/java/ru/job4j/accidents/repository/RuleMem.java
+++ src/main/java/ru/job4j/accidents/repository/RuleMem.java
@@ -3,29 +3,29 @@ package ru.job4j.accidents.repository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Rule;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class RuleMem implements RuleRepository {
 
-    List<Rule> rules = List.of(
-            new Rule(1, "Статья. 1"),
-            new Rule(2, "Статья. 2"),
-            new Rule(3, "Статья. 3")
-    );
+    private final Map<Integer, Rule> rules = new ConcurrentHashMap<>() {
+        {
+            put(1, new Rule(1, "Статья. 1"));
+            put(2, new Rule(2, "Статья. 2"));
+            put(3, new Rule(3, "Статья. 3"));
+        }
+    };
 
     @Override
     public Collection<Rule> findAll() {
-        return rules;
+        return rules.values();
     }
 
     @Override
     public Set<Rule> getRulesByIds(String[] arrayOfIds) {
         Set<Rule> ruleSet = new HashSet<>();
-        for (int i = 0; i < arrayOfIds.length; i++) {
+        for (int i = 1; i <= arrayOfIds.length; i++) {
             ruleSet.add(rules.get(i));
         }
         return ruleSet;
