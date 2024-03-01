0cb2806863df0f6ec7c75924241f4c32024e7fc8
Stepan-Zazyan <stepanzazyan@yandex.ru>
2024-03-01 19:27:11 +0300

    0. Spring Security [#6879]

--- src/main/java/ru/job4j/accidents/repository/AccidentRepositorySpringData.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentRepositorySpringData.java
@@ -1,7 +1,12 @@
 package ru.job4j.accidents.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
 
+import java.util.Optional;
+
+@Repository
 public interface AccidentRepositorySpringData extends JpaRepository<Accident, Integer> {
+
 }
--- src/main/java/ru/job4j/accidents/repository/RuleRepositorySpringData.java
+++ src/main/java/ru/job4j/accidents/repository/RuleRepositorySpringData.java
@@ -3,6 +3,8 @@ package ru.job4j.accidents.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.accidents.model.Rule;
 
+import java.util.Optional;
+
 public interface RuleRepositorySpringData extends JpaRepository<Rule, Integer> {
 }
 
