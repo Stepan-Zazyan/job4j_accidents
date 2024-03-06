774ad4fa5e35fa56b5e7a26aa7e66fa8e2a3ca0a
Stepan-Zazyan <stepanzazyan@yandex.ru>
2024-03-06 12:05:52 +0300

    2. Регистрация пользователя [#296069] registration error fixed

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -2,9 +2,11 @@ package ru.job4j.accidents.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
 import ru.job4j.accidents.repository.UserRepository;
@@ -23,7 +25,12 @@ public class RegController {
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@RequestParam(value = "error", required = false) String error,
+            @ModelAttribute User user, Model model) {
+        if (users.existsByUsername(user.getUsername())) {
+            model.addAttribute("message", "Пользователь с таким именем уже существует.");
+            return "/reg";
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
--- src/main/java/ru/job4j/accidents/repository/UserRepository.java
+++ src/main/java/ru/job4j/accidents/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ru.job4j.accidents.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByUsername(String name);
 }
