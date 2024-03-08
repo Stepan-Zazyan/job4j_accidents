26ce79be96aed966c28c454e5b9fc269d07adcd5
Stepan-Zazyan <stepanzazyan@yandex.ru>
2024-03-08 15:41:01 +0300

    2. Регистрация пользователя [#296069 #484525] userName validation when register

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -2,39 +2,33 @@ package ru.job4j.accidents.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
-import ru.job4j.accidents.repository.UserRepository;
+import ru.job4j.accidents.service.UserService;
 
 @Controller
 public class RegController {
 
     private final PasswordEncoder encoder;
-    private final UserRepository users;
+    private final UserService userService;
     private final AuthorityRepository authorities;
 
-    public RegController(PasswordEncoder encoder, UserRepository users, AuthorityRepository authorities) {
+    public RegController(PasswordEncoder encoder, UserService userService, AuthorityRepository authorities) {
         this.encoder = encoder;
-        this.users = users;
+        this.userService = userService;
         this.authorities = authorities;
     }
 
     @PostMapping("/reg")
-    public String regSave(@RequestParam(value = "error", required = false) String error,
-            @ModelAttribute User user, Model model) {
-        if (users.existsByUsername(user.getUsername())) {
-            model.addAttribute("message", "Пользователь с таким именем уже существует.");
-            return "/reg";
-        }
+    public String regSave(@ModelAttribute User user) {
+        userService.existsByUsername(user.getUsername());
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
+        userService.save(user);
         return "redirect:/login";
     }
 
