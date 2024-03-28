465eff9096b2353b085cbf563505dc955795b674
Stepan-Zazyan <stepanzazyan@yandex.ru>
2024-03-28 22:21:07 +0300

    0. Spring test [#6881 #484925]

--- db/scripts/002_dml_insert_accidents.sql
+++ db/scripts/002_dml_insert_accidents.sql
@@ -1,4 +1,4 @@
-insert into accidents (id, name, text, address, accident_type_id)
-values (1, 'incident 1', 'text1', 'address1', 2),
-       (2, 'incident 2', 'text2', 'address2', 3),
-       (3, 'incident 3', 'text3', 'address3', 1);
+insert into accidents (id, name, text, address)
+values (1, 'incident 1', 'text1', 'address1'),
+       (2, 'incident 2', 'text2', 'address2'),
+       (3, 'incident 3', 'text3', 'address3');
