e4bf8f14ecdbdd64cba03bedc1ce8d10712e40ee
Stepan-Zazyan <stepanzazyan@yandex.ru>
2024-03-29 20:41:36 +0300

    2. Регистрация пользователя [#296069 #484525]

--- src/main/java/ru/job4j/accidents/service/UserService.java
+++ src/main/java/ru/job4j/accidents/service/UserService.java
@@ -2,6 +2,7 @@ package ru.job4j.accidents.service;
 
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.UserRepositorySpringData;
@@ -10,13 +11,19 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepositorySpringData userRepository;
 
     public Optional<User> save(User user) {
-        return userRepository.existsByUsername(user.getUsername())
-                ? Optional.empty() : Optional.of(user);
+        try {
+            userRepository.save(user);
+            return Optional.of(user);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return Optional.empty();
     }
 
 }
